31acbfb1119648108b5a074d1849bae26c903fc8
gunmack <jreedoy2002@yahoo.com>
2024-10-15 04:39:44 -0700

    implemented basic navbar and added css classes to globals.css

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <a className="nbar_items" 
+            href="#" target="_blank">
+            About
+            </a>
+            <a
+            className="nbar_items"
+            href="#" target="_blank">
+            Projects
+            </a>
+            <a
+            className="nbar_items"
+            href="#" target="_blank">
+            Resume
+            </a>
+        </div>
+    )
+}

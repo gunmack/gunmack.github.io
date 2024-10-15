1b8f23b93d6154c01816895be352b1200c89332d
gunmack <jreedoy2002@yahoo.com>
2024-10-15 04:02:40 -0700

    added css classes to globals.css

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
+import "./globals.css";
+
 export default function Landing(){
-    const wrap = "grid grid-rows items-center justify-items-center gap-16 min-h-screen";
-    const body = "flex flex-col gap-8 row-start-1 items-center"
     return(
-        <div className={wrap}>
-            <main className={body}>
+        <div className="wrap">
+            <main className="main">
                 Hello World!
             </main>
         </div>

6b9cf29c1df6af6f499a827b97890178257e3a78
gunmack <jreedoy2002@yahoo.com>
2024-10-15 05:19:40 -0700

    landing page just about done

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,25 @@
 import "./globals.css";
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 export default function Landing(){
     return(
         <div>
+            <Navbar/>
             <div className="wrap">
                 <main className="main">
                     Hello World!
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.
+
                 </main>
             </div>
+            <Footer/>
         </div>
     ) 
 }

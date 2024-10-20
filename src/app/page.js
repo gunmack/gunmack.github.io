6b906892be72b656403425e12854ebc98981eb0c
gunmack <jreedoy2002@yahoo.com>
2024-10-20 06:39:00 -0700

    updated css classes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+function Nav({name}){
+    return(
+      <div>
+        <button className="button">
+          <a href="/about">{name}</a>
+        </button>
+      </div>
+    )
+  }
+  
+
+
 export default function Landing(){
     return(
         <div>
+            <Nav name="About"/>
             <div className="wrap">
                 <main className="main">
                     Hello World!

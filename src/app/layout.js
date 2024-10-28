eea69222b32cf7b06969474584a8fb1f2eb862f1
gunmack <jreedoy2002@yahoo.com>
2024-10-28 01:11:18 -0700

    minor fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
 export const metadata = {
-  title: "Julkar' Website",
+  title: "Julkar's Website",
   image: "favicon.ico",
   description: "My name is Julkar and welcome to my page :)",
 };

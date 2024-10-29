5940abbeee66c857d7d2f8096d315d431202f352
Julkar Reedoy (jreed) <91394056+gunmack@users.noreply.github.com>
2024-10-28 19:36:31 -0700

    added portrait to landing/home page (#45)

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import Link from "next/link";
+import Image from "next/image";
 
 function Header() {
   return <header data-testid="header">Hello there!</header>;
@@ -10,30 +11,41 @@ function Text() {
   const link = "https://www.sfu.ca/outlines.html?2024/fall/cmpt/276/d200";
   const curr_site = "https://www.gunmack.dev/";
   return (
-    <div>
-      <text data-testid="text">
-        <p>
-          My name is Julkar and welcome to my page :)
-          <br />
-          This page was made as part of a lab submission for:
-          <br />
-          <br />
-          <Link className="link" href={link} target="_blank">
-            CMPT 276 Fall 2024 D200
-          </Link>
-          <br />
-          <br />
-          Feel free to explore using the navbar on top. Although this site is
-          <br />
-          not yet complete, If you would like to see my other work:
-          <br />
-          <br />
-          <Link className="link" href={curr_site} target="_blank">
-            👉 Click here
-          </Link>
-        </p>
-      </text>
-    </div>
+    <row_col>
+      <div>
+        <text data-testid="text">
+          <p>
+            My name is Julkar and welcome to my page :)
+            <br />
+            This page was made as part of a lab submission for:
+            <br />
+            <br />
+            <Link className="link" href={link} target="_blank">
+              CMPT 276 Fall 2024 D200
+            </Link>
+            <br />
+            <br />
+            Feel free to explore using the navbar on top. Although this site is
+            <br />
+            not yet complete, If you would like to see my other work:
+            <br />
+            <br />
+            <Link className="link" href={curr_site} target="_blank">
+              👉 Click here
+            </Link>
+          </p>
+        </text>
+      </div>
+      <div>
+        <Image
+          src="/images/grad_pic.png"
+          alt="Julkar"
+          height={250}
+          width={250}
+          className="rounded-lg"
+        />
+      </div>
+    </row_col>
   );
 }
 

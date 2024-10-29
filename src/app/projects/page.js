62b1fccff06dc3adca2999a004ffde202cc02c05
gunmack <jreedoy2002@yahoo.com>
2024-10-28 20:53:45 -0700

    added projects page

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import "@/app/globals.css";
+import Image from "next/image";
 
 function Header() {
   return <header data-testid="header">Projects</header>;
@@ -10,20 +11,88 @@ function Text() {
   const link = "https://github.com/gunmack";
   return (
     <div>
-      <text data-testid="text">
-        <p>
-          While I work on this page,<br></br>
-          feel free to take a look at
+      <p data-testid="text">
+        While I work on this page,<br></br>
+        feel free to take a look at
+        <br />
+        👉{" "}
+        <Link href={link} className="link" target="_blank">
+          my GitHub
+        </Link>
+      </p>
+    </div>
+  );
+}
+
+function Descriptions() {
+  return (
+    <div>
+      <row_col>
+        <div>
+          <a
+            className="link"
+            href="https://github.com/gunmack/Python-Image-Processor"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              className="rounded-lg"
+              src={"/images/project_1.png"}
+              alt="project_1"
+              width={200}
+              height={200}
+            />
+          </a>
           <br />
-          👉{" "}
-          <Link href={link} className="link" target="_blank">
-            my GitHub
-          </Link>
-        </p>
-        <br />
-        <br /> <br />
-        <br />
-      </text>
+          <text>
+            <h3>Python Image Processor</h3>
+            <p>
+              <em>
+                Final project for CMPT 120,
+                <br /> Fall 2021 @ Simon Fraser University
+              </em>
+            </p>
+            <p>
+              <strong>Built with:</strong>
+            </p>
+            <ul>
+              <li>- Numpy</li>
+              <li>- Pygame</li>
+              <li>- Tkinter</li>
+            </ul>
+          </text>
+        </div>
+        <div>
+          <a
+            className="link"
+            href="https://www.gunmack.dev/SFU"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              className="rounded-lg"
+              src={"/images/sfu.png"}
+              alt="project_2"
+              width={500}
+              height={500}
+            />
+          </a>
+          <br />
+          <text>
+            <h3>SFU Campus Timeline</h3>
+            <p>
+              <em>Comprehensive HTML directory of SFU campus buildings</em>
+            </p>
+            <p>
+              <strong>Built with:</strong>
+            </p>
+            <ul>
+              <li> - HTML</li>
+              <li>- Bootstrap CSS</li>
+            </ul>
+          </text>
+        </div>
+      </row_col>
     </div>
   );
 }
@@ -32,6 +101,15 @@ export default function Projects() {
   return (
     <div>
       <Header />
+      <br />
+      <br />
+      <div>
+        <Descriptions />
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
       <Text />
     </div>
   );

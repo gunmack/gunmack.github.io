7939cc54d825881e9bc31f609d2e6c586963cc72
Julkar Reedoy <91394056+gunmack@users.noreply.github.com>
2024-12-11 05:40:58 -0800

    Dev (#71)
    * link fixes
    
    * fixed link to archive
    
    * fixed footer links and added license statement

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,61 +4,68 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="footer_section">
       <footer>
-        <button
-          className="f_items"
-          href="https://www.linkedin.com/in/jreedoy/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Image
-            src={"/images/linkedIn.png"}
-            alt="LinkedIn logo"
-            width={20}
-            height={20}
-          ></Image>
-          LinkedIn
-        </button>
-        <button
-          className="f_items"
-          href="https://github.com/gunmack"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Image
-            className="bg-white rounded-full"
-            src={"/images/gitHub.png"}
-            alt="GitHub logo"
-            width={20}
-            height={20}
-          ></Image>
-          GitHub
-        </button>
-        <a
-          className="link"
-          href="mailto:julkar_reedoy@sfu.ca?subject=Feedback on your GitHub page"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Give Feedback
-        </a>
-        <a
-          className="link"
-          href="https://icons8.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Icons by Icons8
-        </a>
-        <a
-          className="link"
-          href="https://emojipedia.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Emojis from Emojipedia
-        </a>
+        {/* Footer Links Section */}
+        <div className="footer flex flex-wrap">
+          <a
+            className="f_items "
+            href="https://www.linkedin.com/in/jreedoy/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src={"/images/linkedIn.png"}
+              alt="LinkedIn logo"
+              width={20}
+              height={20}
+            />
+            LinkedIn
+          </a>
+          <a
+            className="f_items "
+            href="https://github.com/gunmack"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              className="bg-white rounded-full"
+              src={"/images/gitHub.png"}
+              alt="GitHub logo"
+              width={20}
+              height={20}
+            />
+            GitHub
+          </a>
+          <a
+            className="f_items"
+            href="mailto:julkar_reedoy@sfu.ca?subject=Feedback on your GitHub page"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Give Feedback
+          </a>
+          <a
+            className="f_items"
+            href="https://icons8.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Icons by Icons8
+          </a>
+          <a
+            className="f_items"
+            href="https://emojipedia.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Emojis from Emojipedia
+          </a>
+        </div>
+        {/* Copyright Section */}
+        <div className="pl-2 pb-1 right-0">
+          © 2024 Julkar Reedoy (jreed). All rights reserved.
+        </div>
       </footer>
     </div>
   );
